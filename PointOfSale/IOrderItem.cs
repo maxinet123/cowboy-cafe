dfe0fa9cc02a1698d9f326e110ce958659748a59
maxtex <maxtex@ksu.edu>
2020-02-25 13:05:37 -0600

    started POS2

---
+++ PointOfSale/IOrderItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PointOfSale
+{
+    public interface IOrderItem
+    {
+        public double Price { get; }
+
+        public IEnumerable<string> SpecialInstructions { get; }
+    }
+}
---
+++ PointOfSale/Order.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+
+namespace PointOfSale
+{
+    public class Order : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private uint lastOrderNumber;
+
+        private List<IOrderItem> items = new List<IOrderItem>();
+
+        public IEnumerable<IOrderItem> Items => items.ToArray();
+
+        public double Subtotal { get; }
+
+        public uint OrderNumber { get; }
+
+        public void Add(IOrderItem item)
+        {
+            items.Add(item);
+            //Subtotal += item.Price;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Items"));
+        }
+
+        public void Remove(IOrderItem item)
+        {
+            items.Remove(item);
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Items"));
+        }
+    }
+}
